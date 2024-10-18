cd711cef1d66b230c58acae015aa4320abcdd49b
lramos33 <oliveira.leonardo3004@gmail.com>
2024-10-18 10:36:12 -0300

    chore: add some colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
+import "@/styles/themes.css";
 
 import type { Viewport } from "next";
 
+import { Providers } from "@/contexts/providers";
 import { inter, lexend, manrope } from "@/styles/fonts";
 import { cn } from "@/utils/cn";
 
@@ -13,8 +15,10 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { readonly children: React.ReactNode }) {
   return (
-    <html lang="en" className={cn(inter.variable, lexend.variable, manrope.variable)}>
-      <body>{children}</body>
+    <html lang="en" className={cn(inter.variable, lexend.variable, manrope.variable, "bg-bg-primary text-t-primary")}>
+      <body className="mx-auto min-h-svh w-full max-w-8xl flex-1">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,96 @@ const config: Config = {
         lexend: "var(--font-lexend)",
         inter: "var(--font-inter)",
       },
+
+      fontSize: {
+        xxs: ["0.625rem", "1rem"],
+      },
+
+      maxWidth: {
+        "8xl": "90rem",
+      },
+
+      screens: {
+        xs: "460px",
+        sm: "576px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+      },
+
+      spacing: {
+        4.5: "1.125rem" /* 18px */,
+        5.5: "1.375rem" /* 22px */,
+      },
+
+      colors: {
+        primary: {
+          25: "var(--primary-25)",
+          50: "var(--primary-50)",
+          100: "var(--primary-100)",
+          200: "var(--primary-200)",
+          300: "var(--primary-300)",
+          400: "var(--primary-400)",
+          500: "var(--primary-500)",
+          600: "var(--primary-600)",
+          700: "var(--primary-700)",
+          800: "var(--primary-800)",
+          900: "var(--primary-900)",
+          950: "var(--primary-950)",
+        },
+
+        b: {
+          primary: "var(--border-primary)",
+          secondary: "var(--border-secondary)",
+          tertiary: "var(--border-tertiary)",
+        },
+
+        bg: {
+          primary: "var(--bg-primary)",
+          "primary-hover": "var(--bg-primary-hover)",
+          secondary: "var(--bg-secondary)",
+          "secondary-hover": "var(--bg-secondary-hover)",
+          tertiary: "var(--bg-tertiary)",
+          quaternary: "var(--bg-quaternary)",
+
+          active: "var(--bg-active)",
+          disabled: "var(--bg-disabled)",
+          overlay: "var(--bg-overlay)",
+        },
+
+        t: {
+          primary: "var(--text-primary)",
+          secondary: "var(--text-secondary)",
+          "secondary-hover": "var(--text-secondary-hover)",
+          tertiary: "var(--text-tertiary)",
+          "tertiary-hover": "var(--text-tertiary-hover)",
+          quaternary: "var(--text-quaternary)",
+
+          disabled: "var(--text-disabled)",
+          placeholder: "var(--text-placeholder)",
+        },
+
+        theme: {
+          moss: "#4f7a21",
+          green: "#099250",
+          teal: "#0e9384",
+          cyan: "#088ab2",
+          blue: "#1570ef",
+          indigo: "#444ce7",
+          violet: "#7839ee",
+          purple: "#6938ef",
+          fuchsia: "#ba24d5",
+          pink: "#dd2590",
+          rose: "#e31b54",
+          orange: "#e62e05",
+          yellow: "#ca8504",
+        },
+      },
+
+      borderColor: {
+        DEFAULT: "var(--border-secondary)",
+      },
     },
   },
   plugins: [],
