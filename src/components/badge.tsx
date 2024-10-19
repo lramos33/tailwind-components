8aa88f317b053112940b156a2f3d2097871bc324
lramos33 <oliveira.leonardo3004@gmail.com>
2024-10-19 14:38:30 -0300

    chore: style badge

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -1,3 +1,3 @@
 export function Badge({ children }: { readonly children: React.ReactNode }) {
-  return <div className="rounded-md border border-b-primary px-1.5 py-0.5 text-xs font-medium text-t-secondary">{children}</div>;
+  return <div className="whitespace-nowrap rounded-md border border-b-primary px-1.5 py-0.5 text-xs font-medium text-t-secondary">{children}</div>;
 }
