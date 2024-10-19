f72ef1431df664540ebcf0ac704d88d000cec2c2
lramos33 <oliveira.leonardo3004@gmail.com>
2024-10-19 00:39:12 -0300

    chore: add some tailwind styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,6 +29,8 @@ const config: Config = {
       },
 
       spacing: {
+        4.5: "1.125rem",
+        5.5: "1.375rem",
         18: "4.5rem",
       },
 
@@ -61,6 +63,66 @@ const config: Config = {
           800: "var(--gray-800)",
           900: "var(--gray-900)",
           950: "var(--gray-950)",
+        },
+
+        error: {
+          25: "var(--error-25)",
+          50: "var(--error-50)",
+          100: "var(--error-100)",
+          200: "var(--error-200)",
+          300: "var(--error-300)",
+          400: "var(--error-400)",
+          500: "var(--error-500)",
+          600: "var(--error-600)",
+          700: "var(--error-700)",
+          800: "var(--error-800)",
+          900: "var(--error-900)",
+          950: "var(--error-950)",
+        },
+
+        warning: {
+          25: "var(--warning-25)",
+          50: "var(--warning-50)",
+          100: "var(--warning-100)",
+          200: "var(--warning-200)",
+          300: "var(--warning-300)",
+          400: "var(--warning-400)",
+          500: "var(--warning-500)",
+          600: "var(--warning-600)",
+          700: "var(--warning-700)",
+          800: "var(--warning-800)",
+          900: "var(--warning-900)",
+          950: "var(--warning-950)",
+        },
+
+        success: {
+          25: "var(--success-25)",
+          50: "var(--success-50)",
+          100: "var(--success-100)",
+          200: "var(--success-200)",
+          300: "var(--success-300)",
+          400: "var(--success-400)",
+          500: "var(--success-500)",
+          600: "var(--success-600)",
+          700: "var(--success-700)",
+          800: "var(--success-800)",
+          900: "var(--success-900)",
+          950: "var(--success-950)",
+        },
+
+        info: {
+          25: "var(--info-25)",
+          50: "var(--info-50)",
+          100: "var(--info-100)",
+          200: "var(--info-200)",
+          300: "var(--info-300)",
+          400: "var(--info-400)",
+          500: "var(--info-500)",
+          600: "var(--info-600)",
+          700: "var(--info-700)",
+          800: "var(--info-800)",
+          900: "var(--info-900)",
+          950: "var(--info-950)",
         },
 
         b: {
