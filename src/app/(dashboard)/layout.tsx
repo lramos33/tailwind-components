ee1a50a9f5eed7829440ffa7c9fbc2f6ee04f397
lramos33 <oliveira.leonardo3004@gmail.com>
2024-10-19 13:56:04 -0300

    chore: hide sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { readonly children: React.ReactNode }) {
   return (
     <div className="flex min-h-svh">
-      <div className="flex w-80 justify-center bg-green-950/50 pt-10">sidebar</div>
+      <div className="hidden w-80 justify-center bg-green-950/50 pt-10 lg:flex">sidebar</div>
 
       <div className="flex flex-1 flex-col">
         <header className="flex h-18 items-center justify-center bg-purple-100 dark:bg-purple-950/50">header</header>
