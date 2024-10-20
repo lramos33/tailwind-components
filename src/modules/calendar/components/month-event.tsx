bb981595ed5a1787d4aab6f8fc70edaf1c9985c0
lramos33 <oliveira.leonardo3004@gmail.com>
2024-10-20 11:25:18 -0300

    style

--- src/modules/calendar/components/month-event.tsx
+++ src/modules/calendar/components/month-event.tsx
@@ -9,15 +9,15 @@ const calendarMonthEventVariants = cva(
   {
     variants: {
       variant: {
-        blue: "border-blue-200 bg-blue-700 text-blue-700 dark:border-blue-800 dark:text-blue-300 sm:bg-blue-50 sm:dark:bg-blue-950",
-        green: "border-green-200 bg-green-700 text-green-700 dark:border-green-800 dark:text-green-300 sm:bg-green-50 sm:dark:bg-green-950",
-        red: "border-red-200 bg-red-700 text-red-700 dark:border-red-800 dark:text-red-300 sm:bg-red-50 sm:dark:bg-red-950",
-        yellow: "border-yellow-200 bg-yellow-700 text-yellow-700 dark:border-yellow-800 dark:text-yellow-300 sm:bg-yellow-50 sm:dark:bg-yellow-950",
-        purple: "border-purple-200 bg-purple-700 text-purple-700 dark:border-purple-800 dark:text-purple-300 sm:bg-purple-50 sm:dark:bg-purple-950",
-        gray: "border-gray-200 bg-gray-700 text-gray-700 dark:border-gray-700 dark:text-gray-300 sm:bg-gray-50 sm:dark:bg-gray-900",
+        blue: "border-blue-200 bg-blue-600 text-blue-700 dark:border-blue-800 dark:text-blue-300 sm:bg-blue-50 sm:dark:bg-blue-950",
+        green: "border-green-200 bg-green-600 text-green-700 dark:border-green-800 dark:text-green-300 sm:bg-green-50 sm:dark:bg-green-950",
+        red: "border-red-200 bg-red-600 text-red-700 dark:border-red-800 dark:text-red-300 sm:bg-red-50 sm:dark:bg-red-950",
+        yellow: "border-yellow-200 bg-yellow-600 text-yellow-700 dark:border-yellow-800 dark:text-yellow-300 sm:bg-yellow-50 sm:dark:bg-yellow-950",
+        purple: "border-purple-200 bg-purple-600 text-purple-700 dark:border-purple-800 dark:text-purple-300 sm:bg-purple-50 sm:dark:bg-purple-950",
+        gray: "border-gray-200 bg-gray-600 text-gray-700 dark:border-gray-700 dark:text-gray-300 sm:bg-gray-50 sm:dark:bg-gray-900",
       },
       multiDay: {
-        first: "z-10 mr-0 border sm:mr-0 sm:w-[calc(100%_+_1px)] sm:rounded-r-none sm:border-r-0 lg:mr-0 [&>span]:sm:mr-4.5",
+        first: "z-10 mr-0 sm:mr-0 sm:w-[calc(100%_+_1px)] sm:rounded-r-none sm:border-r-0 lg:mr-0 [&>span]:sm:mr-4.5",
         middle: "z-10 mx-0 sm:mx-0 sm:w-[calc(100%_+_1px)] sm:rounded-none sm:border-x-0 lg:mx-0",
         last: "ml-0 sm:ml-0 sm:rounded-l-none sm:border-l-0 lg:ml-0",
       },
