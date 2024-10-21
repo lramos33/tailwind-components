6120aee1b4270236b2f10d1d0253d49a20573e6d
lramos33 <oliveira.leonardo3004@gmail.com>
2024-10-20 23:11:32 -0300

    chore: fix badge style

--- src/modules/calendar/components/event-badge.tsx
+++ src/modules/calendar/components/event-badge.tsx
@@ -11,17 +11,17 @@ const calendarEventBadgeVariants = cva(
   {
     variants: {
       variant: {
-        blue: "border-blue-200 bg-blue-600 text-blue-700 dark:border-blue-800 dark:text-blue-300 sm:bg-blue-50 sm:dark:bg-blue-950",
-        green: "border-green-200 bg-green-600 text-green-700 dark:border-green-800 dark:text-green-300 sm:bg-green-50 sm:dark:bg-green-950",
-        red: "border-red-200 bg-red-600 text-red-700 dark:border-red-800 dark:text-red-300 sm:bg-red-50 sm:dark:bg-red-950",
-        yellow: "border-yellow-200 bg-yellow-600 text-yellow-700 dark:border-yellow-800 dark:text-yellow-300 sm:bg-yellow-50 sm:dark:bg-yellow-950",
-        purple: "border-purple-200 bg-purple-600 text-purple-700 dark:border-purple-800 dark:text-purple-300 sm:bg-purple-50 sm:dark:bg-purple-950",
-        gray: "border-gray-200 bg-gray-600 text-gray-700 dark:border-gray-700 dark:text-gray-300 sm:bg-gray-50 sm:dark:bg-gray-900",
+        blue: "border-blue-200 bg-blue-50 text-blue-700 dark:border-blue-800 dark:bg-blue-950 dark:text-blue-300",
+        green: "border-green-200 bg-green-50 text-green-700 dark:border-green-800 dark:bg-green-950 dark:text-green-300",
+        red: "border-red-200 bg-red-50 text-red-700 dark:border-red-800 dark:bg-red-950 dark:text-red-300",
+        yellow: "border-yellow-200 bg-yellow-50 text-yellow-700 dark:border-yellow-800 dark:bg-yellow-950 dark:text-yellow-300",
+        purple: "border-purple-200 bg-purple-50 text-purple-700 dark:border-purple-800 dark:bg-purple-950 dark:text-purple-300",
+        gray: "border-gray-200 bg-gray-50 text-gray-700 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300",
       },
       multiDayPosition: {
-        first: "z-10 mr-0 sm:mr-0 sm:w-[calc(100%_+_1px)] sm:rounded-r-none sm:border-r-0 lg:mr-0 [&>span]:sm:mr-2.5",
-        middle: "z-10 mx-0 sm:mx-0 sm:w-[calc(100%_+_1px)] sm:rounded-none sm:border-x-0 lg:mx-0",
-        last: "ml-0 sm:ml-0 sm:rounded-l-none sm:border-l-0 lg:ml-0",
+        first: "z-10 mr-0 w-[calc(100%_+_1px)] rounded-r-none border-r-0 [&>span]:mr-2.5",
+        middle: "z-10 mx-0 w-[calc(100%_+_1px)] rounded-none border-x-0",
+        last: "ml-0 rounded-l-none border-l-0",
         none: "",
       },
     },
@@ -68,7 +68,7 @@ const CalendarEventBadge = React.forwardRef<HTMLDivElement, CalendarEventBadgePr
     return (
       <div ref={ref} className={calendarEventBadgeClasses} {...props}>
         {renderBadgeText && (
-          <p className="hidden flex-1 truncate font-semibold sm:block">
+          <p className="flex-1 truncate font-semibold">
             {eventCurrentDay && (
               <span className="text-xs">
                 Day {eventCurrentDay} of {eventTotalDays} •{" "}
@@ -78,7 +78,7 @@ const CalendarEventBadge = React.forwardRef<HTMLDivElement, CalendarEventBadgePr
           </p>
         )}
 
-        {renderBadgeText && <span className="hidden sm:block">{format(new Date(event.startDate), "h:mm a")}</span>}
+        {renderBadgeText && <span>{format(new Date(event.startDate), "h:mm a")}</span>}
       </div>
     );
   }
